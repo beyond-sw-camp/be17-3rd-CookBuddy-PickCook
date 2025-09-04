4a97bfcbd7191ebff1d18dd0afc3a0bedb6afc4f
seoyoen22 <hsy2709@gmail.com>
2025-09-04 10:19:33 +0900

    레시피 댓글 추가

--- src/main/java/org/example/be17pickcook/domain/recipe/model/RecipeCommentDto.java
+++ src/main/java/org/example/be17pickcook/domain/recipe/model/RecipeCommentDto.java
@@ -3,16 +3,22 @@ package org.example.be17pickcook.domain.recipe.model;
 import lombok.*;
 import org.example.be17pickcook.domain.user.model.User;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RecipeCommentDto {
+
     @Getter
     @Setter
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Request {
-        private String content;
         private Long recipeId;
-        private Long parentRecipeReviewId;
+        private String content;
+        private Long parentCommentId; // 대댓글이면 부모 comment ID
 
         public RecipeComment toEntity(User user, Recipe recipe, RecipeComment parentComment) {
             return RecipeComment.builder()
@@ -31,8 +37,32 @@ public class RecipeCommentDto {
     @NoArgsConstructor
     public static class Response {
         private Long id;
-        private Long recipeId;
-        private Long parentRecipeReviewId;
-    }
+        private String content;
+        private int userId;
+        private String username;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+        private Long parentCommentId;
+        private String imageUrl; // 이미지 URL 하나만
+        private List<Response> children;
 
+        public static Response fromEntity(RecipeComment comment) {
+            return Response.builder()
+                    .id(comment.getId())
+                    .content(comment.getContent())
+                    .userId(comment.getUser().getIdx())
+                    .username(comment.getUser().getNickname())
+                    .createdAt(comment.getCreatedAt())
+                    .updatedAt(comment.getUpdatedAt())
+                    .parentCommentId(comment.getParentComment() != null ? comment.getParentComment().getId() : null)
+                    .imageUrl(comment.getImage() != null ? comment.getImage().getImageUrl() : null)
+                    .children(comment.getChildren() != null ?
+                            comment.getChildren().stream()
+                                    .map(Response::fromEntity)
+                                    .collect(Collectors.toList())
+                            : new ArrayList<>())
+                    .build();
+        }
+    }
 }
+
