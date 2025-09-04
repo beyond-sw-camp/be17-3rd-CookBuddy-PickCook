6cf9561d27943e0be84c0f58a9107b144cdf8a70
seoyoen22 <hsy2709@gmail.com>
2025-09-04 10:19:33 +0900

    레시피 댓글 추가

--- src/main/java/org/example/be17pickcook/domain/recipe/repository/RecipeCommentRepository.java
+++ src/main/java/org/example/be17pickcook/domain/recipe/repository/RecipeCommentRepository.java
@@ -1,7 +1,10 @@
-package org.example.be17pickcook.domain.recipe.service;
+package org.example.be17pickcook.domain.recipe.repository;
 
 import org.example.be17pickcook.domain.recipe.model.RecipeComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecipeCommentRepository extends JpaRepository<RecipeComment, Long> {
+    List<RecipeComment> findByRecipeIdxAndParentCommentIsNullOrderByCreatedAtAsc(Long recipeId);
 }
