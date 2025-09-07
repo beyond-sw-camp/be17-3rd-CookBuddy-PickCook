0a00b09924a0057a3e40e012af3774902eab6a9b
thay123028 <thay123028@naver.com>
2025-09-08 02:46:41 +0900

    마이페이지 게시글 목록에 사용할 게시글 관련 로직2

--- src/main/java/org/example/be17pickcook/domain/community/repository/CommentRepository.java
+++ src/main/java/org/example/be17pickcook/domain/community/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package org.example.be17pickcook.domain.community.repository;
 
 import org.example.be17pickcook.domain.community.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +16,15 @@ public interface CommentRepository extends JpaRepository<Comment,Long> {
 
     // 최상위 댓글 조회
     List<Comment> findByPostIdAndParentCommentIsNull(Long postId);
+
+
+    // 게시글 ID 목록에 대한 댓글 개수 조회
+    @Query("""
+        SELECT c.post.id, COUNT(c.id)
+        FROM Comment c
+        WHERE c.post.id IN :postIds
+        GROUP BY c.post.id
+    """)
+    List<Object[]> countCommentsByPostIds(@Param("postIds") List<Long> postIds);
+
 }
