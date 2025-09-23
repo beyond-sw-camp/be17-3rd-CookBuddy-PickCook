b94103aba472134ba124544149bb70e3859e68eb
seoyoen22 <hsy2709@gmail.com>
2025-09-23 10:27:12 +0900

    test

--- src/main/java/org/example/be17pickcook/TestController.java
+++ src/main/java/org/example/be17pickcook/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test(){
-        return "test03";
+        return "test04";
     }
 
     @GetMapping("/health")
